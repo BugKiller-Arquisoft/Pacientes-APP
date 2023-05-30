5e674b252e8c7495245a99974eea263594ebd7b5
Jonathan1503 <j.riverao@uniandes.edu.co>
2023-05-30 13:57:37 -0500

    Final codigo

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from http import client
-from flask import Flask, request
+from urllib import response
+from flask import Flask, Response, jsonify, request
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
+from bson import ObjectId, json_util
 
 app= Flask(__name__)
 client =MongoClient("mongodb+srv://pacientes-user:isis2503@cluster0.zga6qxp.mongodb.net/?retryWrites=true&w=majority")
@@ -30,6 +32,23 @@ def pacientes():
             'eps':eps,
             'ciudadresidencia':ciudadresidencia
         }
+         
+    return response
+@app.route('/pacientes',methods=['GET'])
+def get_pacientes():
+    pacientes = client["pacientes-db"]["pacientes"].find()
+    response = json_util.dumps(pacientes)
+    
+    return Response(response, mimetype='application/json')
+@app.route('/pacientes/<id>',methods=['GET']) 
+def get_paciente(id):
+    paciente =client["pacientes-db"]["pacientes"].find_one({'_id':ObjectId(id)})
+    response= json_util.dumps(paciente)
+    return Response(response, mimetype='application/json')
+@app.route('/pacientes/<id>',methods=['DELETE'])
+def delete_paciente(id):
+    paciente =client["pacientes-db"]["pacientes"].delete_one({'_id':ObjectId(id)})
+    response = jsonify({'message': 'El paciente con el id: '+ id +' fue eliminado exitosamente'})
     return response
 if __name__ == "__main__":
     app.run(debug=True)
